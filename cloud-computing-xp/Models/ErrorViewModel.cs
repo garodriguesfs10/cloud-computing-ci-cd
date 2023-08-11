363a2c28055b071ab8263fa9cb17e410aede3535
garodriguesfs10 <garodriguesfs10@gmail.com>
2023-08-10 22:01:51 -0300

    first commit

---
+++ cloud-computing-xp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cloud_computing_xp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
